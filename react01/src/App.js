a10c8a82ff68a90f7954486bdb529aa08c04c0e1
huydeptrai <ldmh2042002@gmail.com>
2021-10-09 08:34:33 +0700

    huy chuyen file

---
+++ react01/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HomeComponent from './BaiTapThucHanhLayoutComponent/HomeComponent';
+import BodyComponent from './BaiTapThucHanhLayoutComponent/BodyComponent';
+import FooterComponent from './BaiTapThucHanhLayoutComponent/FooterComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react01/src/BaiTapThucHanhLayoutComponent/HomeComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import BodyComponent from './BodyComponent'
+import FooterComponent from './FooterComponent'
+import HeaderComponent from './HeaderComponent'
+
+
+export default class HomeComponent extends Component {
+    render() {
+        return (
+            <div>
+                <HeaderComponent/>
+                <BodyComponent/>
+                <FooterComponent/>
+            </div>
+        )
+    }
+}
